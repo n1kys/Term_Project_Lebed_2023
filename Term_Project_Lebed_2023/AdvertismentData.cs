9cec4c82e0f152492373d87a7012b71f70cfd48e
n1kys <lebednikita12@gmail.com>
2023-05-20 04:44:11 +0300

    add a class to serialize Data, and another class to work with this dataset

---
+++ Term_Project_Lebed_2023/AdvertismentData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Term_Project_Lebed_2023
+{
+    internal class AdvertismentData
+    {
+    }
+}
--- Term_Project_Lebed_2023/Form1.Designer.cs
+++ Term_Project_Lebed_2023/Form1.Designer.cs
@@ -30,9 +30,31 @@
         {
             tabControl1 = new TabControl();
             tabPage1 = new TabPage();
+            label2 = new Label();
+            dataGridView2 = new DataGridView();
+            groupBox1 = new GroupBox();
+            button5 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            radioButton4 = new RadioButton();
+            radioButton3 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
             tabControl2 = new TabControl();
             tabPage3 = new TabPage();
+            button7 = new Button();
+            radioButton14 = new RadioButton();
+            radioButton13 = new RadioButton();
+            radioButton12 = new RadioButton();
+            radioButton11 = new RadioButton();
+            radioButton10 = new RadioButton();
             tabPage4 = new TabPage();
+            button6 = new Button();
+            radioButton9 = new RadioButton();
+            radioButton8 = new RadioButton();
+            radioButton7 = new RadioButton();
+            radioButton6 = new RadioButton();
+            radioButton5 = new RadioButton();
             tabPage2 = new TabPage();
             button4 = new Button();
             button3 = new Button();
@@ -41,7 +63,11 @@
             dataGridView1 = new DataGridView();
             tabControl1.SuspendLayout();
             tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            groupBox1.SuspendLayout();
             tabControl2.SuspendLayout();
+            tabPage3.SuspendLayout();
+            tabPage4.SuspendLayout();
             tabPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
@@ -58,6 +84,9 @@
             // 
             // tabPage1
             // 
+            tabPage1.Controls.Add(label2);
+            tabPage1.Controls.Add(dataGridView2);
+            tabPage1.Controls.Add(groupBox1);
             tabPage1.Controls.Add(tabControl2);
             tabPage1.Location = new Point(4, 24);
             tabPage1.Name = "tabPage1";
@@ -66,6 +95,112 @@
             tabPage1.TabIndex = 0;
             tabPage1.Text = "Main page";
             tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(14, 235);
+            label2.Name = "label2";
+            label2.Size = new Size(175, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Info about search by parameter:";
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(8, 257);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.RowTemplate.Height = 25;
+            dataGridView2.Size = new Size(874, 207);
+            dataGridView2.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button5);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(radioButton4);
+            groupBox1.Controls.Add(radioButton3);
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Location = new Point(8, 19);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(374, 207);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // button5
+            // 
+            button5.Enabled = false;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Location = new Point(168, 163);
+            button5.Name = "button5";
+            button5.Size = new Size(195, 23);
+            button5.TabIndex = 2;
+            button5.Text = "Accept";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(6, 140);
+            label1.Name = "label1";
+            label1.Size = new Size(51, 15);
+            label1.TabIndex = 6;
+            label1.Text = "Info text";
+            // 
+            // textBox1
+            // 
+            textBox1.Enabled = false;
+            textBox1.Location = new Point(6, 163);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(142, 23);
+            textBox1.TabIndex = 5;
+            // 
+            // radioButton4
+            // 
+            radioButton4.AutoSize = true;
+            radioButton4.Location = new Point(6, 107);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new Size(138, 19);
+            radioButton4.TabIndex = 4;
+            radioButton4.TabStop = true;
+            radioButton4.Text = "Rename file DataBase";
+            radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(6, 82);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(128, 19);
+            radioButton3.TabIndex = 3;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "Delete file DataBase";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(6, 57);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(115, 19);
+            radioButton2.TabIndex = 2;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Edit file DataBase";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(6, 32);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(163, 19);
+            radioButton1.TabIndex = 1;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Create a new file DataBase";
+            radioButton1.UseVisualStyleBackColor = true;
             // 
             // tabControl2
             // 
@@ -79,6 +214,12 @@
             // 
             // tabPage3
             // 
+            tabPage3.Controls.Add(button7);
+            tabPage3.Controls.Add(radioButton14);
+            tabPage3.Controls.Add(radioButton13);
+            tabPage3.Controls.Add(radioButton12);
+            tabPage3.Controls.Add(radioButton11);
+            tabPage3.Controls.Add(radioButton10);
             tabPage3.Location = new Point(4, 24);
             tabPage3.Name = "tabPage3";
             tabPage3.Padding = new Padding(3);
@@ -87,8 +228,80 @@
             tabPage3.Text = "Search page";
             tabPage3.UseVisualStyleBackColor = true;
             // 
+            // button7
+            // 
+            button7.Enabled = false;
+            button7.FlatStyle = FlatStyle.Flat;
+            button7.Location = new Point(6, 151);
+            button7.Name = "button7";
+            button7.Size = new Size(455, 35);
+            button7.TabIndex = 5;
+            button7.Text = "Search";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton14
+            // 
+            radioButton14.AutoSize = true;
+            radioButton14.Location = new Point(18, 121);
+            radioButton14.Name = "radioButton14";
+            radioButton14.Size = new Size(150, 19);
+            radioButton14.TabIndex = 4;
+            radioButton14.TabStop = true;
+            radioButton14.Text = "Search by info about ad";
+            radioButton14.UseVisualStyleBackColor = true;
+            // 
+            // radioButton13
+            // 
+            radioButton13.AutoSize = true;
+            radioButton13.Location = new Point(18, 96);
+            radioButton13.Name = "radioButton13";
+            radioButton13.Size = new Size(143, 19);
+            radioButton13.TabIndex = 3;
+            radioButton13.TabStop = true;
+            radioButton13.Text = "Search by permit, date";
+            radioButton13.UseVisualStyleBackColor = true;
+            // 
+            // radioButton12
+            // 
+            radioButton12.AutoSize = true;
+            radioButton12.Location = new Point(18, 71);
+            radioButton12.Name = "radioButton12";
+            radioButton12.Size = new Size(126, 19);
+            radioButton12.TabIndex = 2;
+            radioButton12.TabStop = true;
+            radioButton12.Text = "Search by ad's type";
+            radioButton12.UseVisualStyleBackColor = true;
+            // 
+            // radioButton11
+            // 
+            radioButton11.AutoSize = true;
+            radioButton11.Location = new Point(18, 46);
+            radioButton11.Name = "radioButton11";
+            radioButton11.Size = new Size(205, 19);
+            radioButton11.TabIndex = 1;
+            radioButton11.TabStop = true;
+            radioButton11.Text = "Search by contract's number, date";
+            radioButton11.UseVisualStyleBackColor = true;
+            // 
+            // radioButton10
+            // 
+            radioButton10.AutoSize = true;
+            radioButton10.Location = new Point(18, 21);
+            radioButton10.Name = "radioButton10";
+            radioButton10.Size = new Size(164, 19);
+            radioButton10.TabIndex = 0;
+            radioButton10.TabStop = true;
+            radioButton10.Text = "Search by name of subject";
+            radioButton10.UseVisualStyleBackColor = true;
+            // 
             // tabPage4
             // 
+            tabPage4.Controls.Add(button6);
+            tabPage4.Controls.Add(radioButton9);
+            tabPage4.Controls.Add(radioButton8);
+            tabPage4.Controls.Add(radioButton7);
+            tabPage4.Controls.Add(radioButton6);
+            tabPage4.Controls.Add(radioButton5);
             tabPage4.Location = new Point(4, 24);
             tabPage4.Name = "tabPage4";
             tabPage4.Padding = new Padding(3);
@@ -96,6 +309,69 @@
             tabPage4.TabIndex = 1;
             tabPage4.Text = "Sort page";
             tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Enabled = false;
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.Location = new Point(6, 151);
+            button6.Name = "button6";
+            button6.Size = new Size(455, 35);
+            button6.TabIndex = 5;
+            button6.Text = "Sort";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton9
+            // 
+            radioButton9.AutoSize = true;
+            radioButton9.Location = new Point(18, 121);
+            radioButton9.Name = "radioButton9";
+            radioButton9.Size = new Size(136, 19);
+            radioButton9.TabIndex = 4;
+            radioButton9.Text = "Sort by info about ad";
+            radioButton9.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            radioButton8.AutoSize = true;
+            radioButton8.Location = new Point(18, 96);
+            radioButton8.Name = "radioButton8";
+            radioButton8.Size = new Size(129, 19);
+            radioButton8.TabIndex = 3;
+            radioButton8.Text = "Sort by permit, date";
+            radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            radioButton7.AutoSize = true;
+            radioButton7.Location = new Point(18, 71);
+            radioButton7.Name = "radioButton7";
+            radioButton7.Size = new Size(112, 19);
+            radioButton7.TabIndex = 2;
+            radioButton7.Text = "Sort by ad's type";
+            radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            radioButton6.AutoSize = true;
+            radioButton6.Location = new Point(18, 46);
+            radioButton6.Name = "radioButton6";
+            radioButton6.Size = new Size(191, 19);
+            radioButton6.TabIndex = 1;
+            radioButton6.Text = "Sort by contract's number, date";
+            radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            radioButton5.AutoSize = true;
+            radioButton5.Checked = true;
+            radioButton5.Location = new Point(18, 21);
+            radioButton5.Name = "radioButton5";
+            radioButton5.Size = new Size(150, 19);
+            radioButton5.TabIndex = 0;
+            radioButton5.TabStop = true;
+            radioButton5.Text = "Sort by name of subject";
+            radioButton5.UseVisualStyleBackColor = true;
             // 
             // tabPage2
             // 
@@ -114,6 +390,7 @@
             // 
             // button4
             // 
+            button4.FlatStyle = FlatStyle.Flat;
             button4.Location = new Point(240, 399);
             button4.Name = "button4";
             button4.Size = new Size(230, 55);
@@ -123,6 +400,7 @@
             // 
             // button3
             // 
+            button3.FlatStyle = FlatStyle.Flat;
             button3.Location = new Point(476, 399);
             button3.Name = "button3";
             button3.Size = new Size(203, 55);
@@ -132,6 +410,7 @@
             // 
             // button2
             // 
+            button2.FlatStyle = FlatStyle.Flat;
             button2.Location = new Point(685, 399);
             button2.Name = "button2";
             button2.Size = new Size(201, 55);
@@ -141,6 +420,7 @@
             // 
             // button1
             // 
+            button1.FlatStyle = FlatStyle.Flat;
             button1.Location = new Point(8, 399);
             button1.Name = "button1";
             button1.Size = new Size(226, 55);
@@ -168,7 +448,15 @@
             Text = "DataBase Assistance";
             tabControl1.ResumeLayout(false);
             tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             tabControl2.ResumeLayout(false);
+            tabPage3.ResumeLayout(false);
+            tabPage3.PerformLayout();
+            tabPage4.ResumeLayout(false);
+            tabPage4.PerformLayout();
             tabPage2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
@@ -187,5 +475,27 @@
         private TabControl tabControl2;
         private TabPage tabPage3;
         private TabPage tabPage4;
+        private GroupBox groupBox1;
+        private Button button5;
+        private Label label1;
+        private TextBox textBox1;
+        private RadioButton radioButton4;
+        private RadioButton radioButton3;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private DataGridView dataGridView2;
+        private Label label2;
+        private Button button6;
+        private RadioButton radioButton9;
+        private RadioButton radioButton8;
+        private RadioButton radioButton7;
+        private RadioButton radioButton6;
+        private RadioButton radioButton5;
+        private Button button7;
+        private RadioButton radioButton14;
+        private RadioButton radioButton13;
+        private RadioButton radioButton12;
+        private RadioButton radioButton11;
+        private RadioButton radioButton10;
     }
 }
